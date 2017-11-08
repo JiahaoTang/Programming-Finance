b23f314b6501b43f0dec87d0b27c26653c562318
jiahaotang <jiahaotang1994@gmail.com>
2017-11-08 00:21:39 -0500

    Add the output function and vector of packages

--- homework9/homework9/main.cpp
+++ homework9/homework9/main.cpp
@@ -5,33 +5,51 @@
 #include "OvernightPackage.h"
 #include <string>
 #include <iomanip>
+#include <vector>
 
 using namespace std;
 
+void outputInfo (Package*);
+
 int main() {
-	User sender = User("John Smith", "1 Davidson Road", "Piscataway", "NJ", "08854");
-	User receiver = User("Tom Smith", "2 Davidson Road", "Piscataway", "NJ", "08854");
-	Package pkg = TwoDayPackage(sender, receiver, 2, 0.5, 5);
-	
+	User JohnSmith = User("John Smith", "1 Davidson Road", "Piscataway", "NJ", "08854");
+	User TomSmith = User("Tom Smith", "2 Davidson Road", "Piscataway", "NJ", "08854");
+	User MarySmith = User("MarySmith", "3 Davidson Road", "Piscataway", "NJ", "08854");
+	User JenniferSmith = User("Jennifer Smith", "4 Davidson Road", "Piscataway", "NJ", "08854");
+
+	vector< Package * > vectorOfPackage(3);
+
+	vectorOfPackage[0] = &Package(JohnSmith, TomSmith, 10, 0.5);
+	vectorOfPackage[1] = &TwoDayPackage(MarySmith, JenniferSmith, 5, 0.5, 2);
+	vectorOfPackage[2] = &OvernightPackage(JohnSmith, MarySmith, 2, 0.5, 5);
+
+	/*for (int i = 0; i < 3; i++) {
+		cout << "Package " << i + 1 << ":" << endl;
+		cout << endl;
+		outputInfo(vectorOfPackage[i]);
+	}*/
+	cout << vectorOfPackage[0]->getSender().getName() << endl;
+	system("Pause");
+	return 0;
+}
+
+void outputInfo (Package* pkg) {
 	cout << "Sender:" << endl;
-	cout << pkg.getSender().getName() << endl;
-	cout << pkg.getSender().getAddress() << endl;
-	cout << pkg.getSender().getCity() << ", " << pkg.getSender().getState() << " " << pkg.getSender().getZipCode() << endl;
+	cout << (*pkg).getSender().getName() << endl;
+	cout << (*pkg).getSender().getAddress() << endl;
+	cout << (*pkg).getSender().getCity() << ", " << (*pkg).getSender().getState() << " " << (*pkg).getSender().getZipCode() << endl;
 	cout << endl;
 
 	cout << "Receiver:" << endl;
-	cout << pkg.getReceiver().getName() << endl;
-	cout << pkg.getReceiver().getAddress() << endl;
-	cout << pkg.getReceiver().getCity() << ", " << pkg.getReceiver().getState() << " " << pkg.getReceiver().getZipCode() << endl;
+	cout << (*pkg).getReceiver().getName() << endl;
+	cout << (*pkg).getReceiver().getAddress() << endl;
+	cout << (*pkg).getReceiver().getCity() << ", " << (*pkg).getReceiver().getState() << " " << (*pkg).getReceiver().getZipCode() << endl;
 	cout << endl;
 
-	cout << "Weight of package: " << pkg.getWeight() << " ounces" << endl;
-	cout << "Type of delivery: " << pkg.getType() << endl;
+	cout << "Weight of package: " << (*pkg).getWeight() << " ounces" << endl;
+	cout << "Type of delivery: " << (*pkg).getType() << endl;
 	cout.precision(2);
 	cout.setf(ios::fixed);
-	cout << "Cost of Package: $" << pkg.calculateCost() << endl;
+	cout << "Cost of Package: $" << (*pkg).calculateCost() << endl;
 	cout << endl;
-
-	system("Pause");
-	return 0;
 }
